be7ade16116b7a0baf1178d95275fba0332a88eb
Cinarrrddqq <112159362+Cinarrrddqq@users.noreply.github.com>
2022-09-28 20:36:36 +0400

    update ping.py

--- handlers/ping.py
+++ handlers/ping.py
@@ -1,1 +1,39 @@
-Skdkskdkd
+import os
+import time
+from datetime import datetime
+
+import psutil
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from import BOT_USERNAME, MUSIC_BOT_NAME, app, boottime
+
+
+
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - boottime)
+    cpu = psutil.cpu_percent(interval=0.5)
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    stats = f"""
+ᴜᴘᴛɪᴍᴇ: {get_readable_time((bot_uptime))}
+ᴄᴘᴜ: {cpu}%
+ʀᴀᴍ: {mem}%
+ᴅɪsᴋ: {disk}%"""
+    return stats
+
+
+@app.on_message(filters.command(["ping", f"ping@{BOT_USERNAME}"]))
+async def ping(_, message):
+    start = datetime.now()
+    response = await message.reply_photo(
+        photo="https://telegra.ph/file/ae932688f053e8464d931.jpg",
+        caption="🌸 ᴘɪɴɢ...",
+    )
+    uptime = await bot_sys_stats()
+    end = datetime.now()
+    resp = (end - start).microseconds / 1000
+    await response.edit_text(
+        f"**💐 ᴘᴏɴɢ**\n`⚡{resp} ᴍs`\n\n**{MUSIC_BOT_NAME} sʏsᴛᴇᴍ sᴛᴀᴛs:**{uptime}"
+    )
+
