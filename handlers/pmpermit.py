846f5e7c30360b60056fc84616cdb279ee4bf476
riyad022tagiyevv <97640853+riyad022tagiyevv@users.noreply.github.com>
2023-03-07 14:34:12 +0400

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -18,7 +18,7 @@ async def pmPermit(client: USER, message: Message):
                 return
             await USER.send_message(
                 message.chat.id,
-                "Salam, Mən @LegendMucisBot - un Asistanıyam [Developer](https://t.me/ElnurGenCeLi) 🇦🇿\n",
+                "Salam, Mən @N1MucisBot - un Asistanıyam [Developer](https://t.me/RiyadAndMe) 🇦🇿\n",
             )
             return
  
